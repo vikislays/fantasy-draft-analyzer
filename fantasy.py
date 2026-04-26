50de2ec43e6dfa1d2890b3f237d428b7182a364f
vikislays <victoria.benko@gmail.com>
2026-04-26 23:35:20 +0000

    Added player_stats and implemented find_player.

--- fantasy.py
+++ fantasy.py
@@ -1,18 +1,33 @@
+import csv
+
+players = []
+
 def print_menu():
     print("---Main Menu---")
     print("1 - Print Player Statistics")
     print("0 - Quit")
 
+def load_file():
+    with open('nfl_player_stats.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            players.append(row)
 
+def find_player(player_name):
+    for row in players:
+        if row[0] == player_name:
+            print(row)
 
 def main():
+    load_file()
     while True:
         try:
             print_menu()
             menu_selection = int(input("Enter [1-6] for the menu option you "
                                         "would like to select: "))
             if menu_selection == 1:
-                pass
+                player_name = input("Enter player name: ")
+                find_player(player_name)
             elif menu_selection == 0:
                 break
             else:
