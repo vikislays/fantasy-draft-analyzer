c8145c5f0d8f202328ab2be8bdbcafd1d49b56ca
vikislays <victoria.benko@gmail.com>
2026-04-27 02:08:00 +0000

    Implemented function sum_player_totals.

--- fantasy.py
+++ fantasy.py
@@ -1,17 +1,19 @@
 import csv
 
-players = []
-
 def print_menu():
     print("---Main Menu---")
     print("1 - Print Player Statistics")
+    print("2 - Top Ten Players by Points")
     print("0 - Quit")
 
 def load_file():
+    players = []
     with open('nfl_player_stats.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
         for row in reader:
             players.append(row)
+    return players
 
 def print_player_card(player_name, position, games):
     print("===============================")
@@ -35,7 +37,7 @@ def print_player_card(player_name, position, games):
     print(f"Games played: {games_played}")
     print(f"Points per game: {average}")
 
-def find_player(player_name):
+def find_player(players, player_name):
     games = list()
     for row in players:
         if row[0] == player_name:
@@ -45,18 +47,31 @@ def find_player(player_name):
          
     print_player_card(player_name, position, games)
 
+def sum_player_totals(players):
+    player_totals = {}
+    for row in players:
+        player_name = row[0]
+        points = float(row[3]) if row[3] else 0  
+        if player_name in player_totals:
+            player_totals[player_name] += points
+        else:
+            player_totals[player_name] = points
+
+    return player_totals        
 
 def main():
-    load_file()
+    players = load_file()
     while True:
         try:
             print_menu()
-            menu_selection = int(input("Enter [1-6] for the menu option you "
-                                        "would like to select: "))
+            menu_selection = int(input("Enter [1-6] for the menu option you would like to select: "))
+            
             if menu_selection == 1:
                 player_name = input("Enter player name: ")
                 player_name = "Cade Otton"
-                find_player(player_name)
+                find_player(players, player_name)
+            elif menu_selection == 2:
+                player_totals = sum_player_totals(players)
             elif menu_selection == 0:
                 break
             else:
