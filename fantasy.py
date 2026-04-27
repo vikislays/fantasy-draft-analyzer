f79314109fddac02570b69d9ef86c12ab558e813
vikislays <victoria.benko@gmail.com>
2026-04-27 00:32:39 +0000

    Implemented print_player_card.

--- fantasy.py
+++ fantasy.py
@@ -13,10 +13,38 @@ def load_file():
         for row in reader:
             players.append(row)
 
+def print_player_card(player_name, position, games):
+    print("===============================")
+    print(f"{player_name}, {position}")
+    print("===============================")
+    total_points = 0
+    games_played = 0
+    for game in games:
+        points = game[1]
+        if points:
+            total_points += float(points)
+            games_played += 1
+            print(f"GW {game[0]}: {points} pts")
+        else:
+            print(f"GW {game[0]}: BYE")
+        
+    average = round(total_points/games_played, 1)
+    
+    print("===============================")
+    print(f"Total points: {total_points}")
+    print(f"Games played: {games_played}")
+    print(f"Points per game: {average}")
+
 def find_player(player_name):
+    games = list()
     for row in players:
         if row[0] == player_name:
-            print(row)
+            position = row[1]
+            games.append((row[2], row[3]))
+
+         
+    print_player_card(player_name, position, games)
+
 
 def main():
     load_file()
@@ -27,6 +55,7 @@ def main():
                                         "would like to select: "))
             if menu_selection == 1:
                 player_name = input("Enter player name: ")
+                player_name = "Cade Otton"
                 find_player(player_name)
             elif menu_selection == 0:
                 break
